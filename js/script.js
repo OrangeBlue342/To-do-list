af3863de2e86463364ca2937246875451825025e
Joanna Czub <joannaczub53@gmail.com>
2022-12-11 10:46:53 +0100

    js added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 {
+    const tasks = [
+        {
+            content: "nagrać lekcję",
+            done: false,
+        },
+        {
+            content: "zjeść pierogi",
+            done: true,
+        },
+    ];
     
+    const init = () => {
 
-    
+    };
+
+    init();
 }
     
