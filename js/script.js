ca0a4770d54fcbca699981dbe9ee7d77577d4770
Joanna Czub <joannaczub53@gmail.com>
2022-12-11 10:58:12 +0100

    js changed

--- js/script.js
+++ js/script.js
@@ -9,9 +9,23 @@
             done: true,
         },
     ];
+
+    const render = () => {
+       let htmlString = "";
+
+       for (const task of tasks) {
+        htmlString += '
+        <li>
+          ${task.content}
+        </li>
+        ';
+       }
+
+       document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
     
     const init = () => {
-
+        render();
     };
 
     init();
