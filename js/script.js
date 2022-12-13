b31e25d7f6491c2a5abf91feb72e743ac9e8ea11
Joanna Czub <joannaczub53@gmail.com>
2022-12-13 15:15:37 +0100

    functions added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 {
     const tasks = [
-        {
-            content: "nagrać lekcję",
-            done: false,
-        },
-        {
-            content: "zjeść pierogi",
-            done: true,
-        },
+        
     ];
+
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+
+        render();
+    }
 
     const render = () => {
        let htmlString = "";
@@ -17,18 +17,60 @@
         htmlString += `
         <li class="list_item">
         <button class="button_done js-done">
+        ${task.done ? "&#10004;" : ""}
         </button>
+        <span class="${task.done ? "list_item_done" : ""}">
           ${task.content};
-          <button class="button_remove js-remove"></button>
+          </span>
+          <button class="button_remove js-remove">&#128465</button>
         </li>
-        `
-       };
+        `;
+       }
 
        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+       const removeButtons = document.querySelectorAll(".js-remove");
+
+       removeButtons.forEach((removeButton, index) => {
+         removeButton.addEventListener("click", () => {
+          tasks.splice(index, 1);
+          render();
+         });
+       })
     };
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+        toggleDoneButton.addEventListener("click", () => {
+            toggleTaskDone(index);
+        })
+    });
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+          });
+
+          render();
+    }
     
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+          event.preventDefault();
+
+          const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+          if(newTaskContent === "") {
+            return;
+          }
+
+          addNewTask(newTaskContent);
+        });
     };
 
     init();
