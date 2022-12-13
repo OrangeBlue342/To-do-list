8a1428bfa74680901f895d807be51920bf29657b
Joanna Czub <joannaczub53@gmail.com>
2022-12-13 13:12:07 +0100

    List item styled

--- js/script.js
+++ js/script.js
@@ -14,12 +14,15 @@
        let htmlString = "";
 
        for (const task of tasks) {
-        htmlString += '
-        <li>
-          ${task.content}
+        htmlString += `
+        <li class="list_item">
+        <button class="button_done js-done">
+        </button>
+          ${task.content};
+          <button class="button_remove js-remove"></button>
         </li>
-        ';
-       }
+        `
+       };
 
        document.querySelector(".js-tasks").innerHTML = htmlString;
     };
